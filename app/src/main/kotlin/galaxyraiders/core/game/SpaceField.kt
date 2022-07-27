838dd929aa81736bca21483e71666feb6289ff6d
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-07-27 03:45:09 -0400

    Revert debug purpose changes in SpaceField class
    Co-authored-by: Jose Lucas Silva Mayer <josemayer@usp.br>
    Co-authored-by: Eduardo Ribeiro Silva de Oliveira <edusillva784@gmail.com>

--- app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
@@ -96,9 +96,8 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
   }
 
   private fun standardShipPosition(): Point2D {
-    return Point2D(x = 1.0, y = 1.0)
+    return Point2D(x = this.width / 2.0, y = 1.0)
   }
-
 
   private fun standardShipVelocity(): Vector2D {
     return Vector2D(dx = 0.0, dy = 0.0)
@@ -114,13 +113,11 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
   }
 
   private fun defineMissilePosition(missileRadius: Double): Point2D {
-    return ship.center + Vector2D(
-      dx = ship.radius + missileRadius + SpaceFieldConfig.missileDistanceFromShip,
-      dy = 0.0)
+    return ship.center + Vector2D(dx = 0.0, dy = ship.radius + missileRadius + SpaceFieldConfig.missileDistanceFromShip)
   }
 
   private fun defineMissileVelocity(): Vector2D {
-    return Vector2D(dx = 1.0, dy = 0.0)
+    return Vector2D(dx = 0.0, dy = 1.0)
   }
 
   private fun createAsteroidWithRandomProperties(): Asteroid {
